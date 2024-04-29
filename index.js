2d7e73de328331ad3d92f9d86c001274daa25618
mr-codex <mcodex47@gmail.com>
2024-04-29 23:20:56 +0600

    delete method added

--- index.js
+++ index.js
@@ -136,6 +136,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/spots/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpots.deleteOne(query)
+      res.send(result)
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
