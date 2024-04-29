dbb995d4c798787cf47121d2dacfdc0d93761fa5
mr-codex <mcodex47@gmail.com>
2024-04-29 07:08:11 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,105 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("this is server of assignment 10");
 });
 
+// const uri =
+//   "mongodb+srv://dreamVenture:toF9RU27DE6O9iYS@cluster0.tyigyp7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tyigyp7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const database = client.db("dreamVenture");
+    const touristSpots = database.collection("touristSpots");
+
+    app.get("/spots", async (req, res) => {
+      const cursor = touristSpots.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/spots/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpots.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/addSpot", async (req, res) => {
+      const {
+        SpotName,
+        Location,
+        Photo,
+        Description,
+        Country,
+        Cost,
+        Season,
+        Traveltime,
+        Visitor,
+        Email,
+        UserName,
+      } = req.body;
+      console.log(
+        SpotName,
+        Location,
+        Photo,
+        Description,
+        Country,
+        Cost,
+        Season,
+        Traveltime,
+        Visitor,
+        Email,
+        UserName
+      );
+      const touristSpot = {
+        SpotName,
+        Location,
+        Photo,
+        Description,
+        Country,
+        Cost,
+        Season,
+        Traveltime,
+        Visitor,
+        Email,
+        UserName,
+      };
+      const result = await touristSpots.insertOne(touristSpot);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
